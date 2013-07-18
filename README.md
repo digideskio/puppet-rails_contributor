8877ceee8de23ffb3f2267c4c61af40724355a31
Rafael Mendonça França <rafael.franca@plataformatec.com.br>
2013-07-18 04:27:22 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Rails contributor
+
+A puppet module for developing the Rails project.
+
+# Required Modules for Rails contributor
+
+* boxen
+* repository
+* homebrew
+* stdlib
+* sysctl
+* postgresql
+* mysql
+* gcc
+* openssl
+* ruby
+
+## Usage
+
+```puppet
+include rails_contributor
+```
+
+## Development
+
+Write code. Check the `script` directory for other useful tools.
---
+++ manifests/init.pp
@@ -0,0 +1,29 @@
+# Public: Configure a development environment to contribute to the Rails project
+#
+# Usage:
+#
+#   include rails_contributor
+class rails_contributor {
+  include boxen::config
+
+  $dir = "${boxen::config::srcdir}/rails"
+
+  include postgresql
+  include mysql
+  include ruby::1_8_7
+  include ruby::1_9_3
+  include ruby::2_0_0
+
+  file { $dir:
+    ensure => directory
+  }
+
+  $repos = rails_repos()
+  rails_contributor::project { $repos: }
+
+  file { "${dir}/rails/activerecord/test/config.yml":
+    source  => 'puppet:///modules/puppet-rails_contributor/config.yml',
+    ensure  => present,
+    require => Rails_Contributor::Project['rails']
+  }
+}
